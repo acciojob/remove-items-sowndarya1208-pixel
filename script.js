483ac6f975bae0a7760c94e64926e3afc4fb1ff2
sowndarya1208-pixel <sowndarya1208@gmail.com>
2025-12-14 16:57:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+ function removeColor() {
+            const select = document.getElementById("colorSelect");
+            const selectedIndex = select.selectedIndex;
+
+            if (selectedIndex !== -1) {
+                select.remove(selectedIndex);
+            }
+        }
